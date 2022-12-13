a92c27d62ffc8886ec201a08d54a0703dea5801c
R <misterbola74@gmail.com>
2022-12-13 09:08:15 +0700

    Add files via upload

---
+++ La1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php
+    $txt1 = "Belajar PHP";
+    $txt2 = "Pemrograman Web";
+    $x = 5;
+    $y = 4;
+    
+    echo "<h2>" .$txt1. "</h2>";
+    echo "Pada Mata Kuliah" .$txt2. "<br>";
+    echo $y + $x;
+    ?>
+    
+  </body>
+</html>
